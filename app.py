ba38b40b498d846d16ae37ec5d6c8a4fe930b12f
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2024-06-22 07:18:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,28 +3,28 @@ from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
-# Specify canvas parameters in application
-drawing_mode = st.sidebar.selectbox(
-    "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
-)
+# # Specify canvas parameters in application
+# drawing_mode = st.sidebar.selectbox(
+#     "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
+# )
 
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-if drawing_mode == 'point':
-    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+# stroke_width = 10
+# if drawing_mode == 'point':
+#     point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+# stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+# bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+# bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
 
-realtime_update = st.sidebar.checkbox("Update in realtime", True)
+# realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
     
 
 # Create a canvas component
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    background_color=bg_color,
+    stroke_width=10,
+    stroke_color=10,
+    background_color=0,
     background_image=Image.open(bg_image) if bg_image else None,
     update_streamlit=realtime_update,
     height=150,
