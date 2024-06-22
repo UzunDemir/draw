8ab35ad16dd8fae78d9d28aa58a2527cc59a6af5
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2024-06-22 07:27:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ drawing_mode = st.sidebar.selectbox(
     "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
 )
 
-stroke_width = 10
+stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
 if drawing_mode == 'point':
     point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
 stroke_color = st.sidebar.color_picker("Stroke color hex: ")
@@ -22,13 +22,13 @@ realtime_update = st.sidebar.checkbox("Update in realtime", True)
 # Create a canvas component
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=10,
-    stroke_color=10,
-    background_color=0,
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
     background_image=Image.open(bg_image) if bg_image else None,
     update_streamlit=realtime_update,
     height=150,
-    drawing_mode="freedraw",
+    drawing_mode=drawing_mode,
     point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
     key="canvas",
 )
